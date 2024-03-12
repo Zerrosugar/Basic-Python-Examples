868c2f4cb451de86a1254c46b56c7909e8b949c3
Zerrosugar <150507640+Zerrosugar@users.noreply.github.com>
2024-03-12 16:16:12 +0100

    A regular python code using functions.
    This is a code suitable for those learning Python, right down to the functions.A simple interactive calculator program.

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def summation(a, b):
+    print(a+b)
+    return (a+b)
+
+
+def extraction(a, b):
+    print(a-b)
+    return (a-b)
+
+
+def divide(a, b):
+    print(a/b)
+    return (a/b)
+
+
+def multiplication(a, b):
+    print(a*b)
+    return (a*b)
+# This is the functions.
+
+
+operation = int(
+    input("summation = 1\nextraction = 2\ndivide = 3\nmultiplication = 4\n"))
+# We asked the operation.
+
+if (operation == 1):
+    a = int(input("What's a "))
+    b = int(input("What's b "))
+    summation(a, b)
+
+elif (operation == 2):
+    a = int(input("What's a "))
+    b = int(input("What's b "))
+    extraction(a, b)
+
+elif (operation == 3):
+    a = int(input("What's a "))
+    b = int(input("What's b "))
+    divide(a, b)
+
+elif (operation == 4):
+    a = int(input("What's a "))
+    b = int(input("What's b "))
+    multiplication(a, b)
+
+else:
+    print("Please, enter a defined number.")
+# We did the operation using the If-Else structure.
